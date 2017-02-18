485bbaa5fbfcbe5902431da56031a84e15dbc710
tkanen <tina.kane@gmail.com>
2017-02-18 12:16:42 -0800

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,11 @@ class Scraper
 
     student_scraper = []
 
+    index_url.css("div.student_card").each do |student|
+      name = student.css("h4.student-name").text
+      location = student.css("p.student-location")
+    end
+    student_scraper
   end
 
   def self.scrape_profile_page(profile_url)
